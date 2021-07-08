0f837309703f53d63eea4f3e860612badf438aa4
ANDRKORN <andrey.kornienko.1997@mail.ru>
2021-07-08 21:21:52 +0300

    Create components and css-module

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import head_style from'./Header.module.css'; 
+const Header = () => {
+    return <header className={head_style.header}>
+        <img src='https://www.freelogodesign.org/Content/img/logo-ex-7.png' />
+    </header>
+}
+
+export default Header;
